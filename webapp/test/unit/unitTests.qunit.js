6ad4e24d293151e135bcb66052a11a57012d6fdd
nikola.dmitrasinovic <nikola.dm@yahoo.com>
2023-11-16 14:18:21 +0100

    Step 27: Unit Test with QUnit

---
+++ webapp/test/unit/unitTests.qunit.js
@@ -0,0 +1,9 @@
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(() => {
+  "use strict";
+
+  sap.ui.require(["ui5/walkthrough/test/unit/model/formatter"], () =>
+    QUnit.start()
+  );
+});
